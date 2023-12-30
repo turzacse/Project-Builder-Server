fbc11f16c2a6146be3b04ff4b5c7bf735adba485
turzacse <turzacse@gmail.com>
2023-12-30 22:39:51 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5001;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnzewy6.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const projectCollection = client.db('ProjectDB').collection('products');
+
+    //read the product data from the database 
+    app.get('/project', async (req, res) => {
+      const cursor = projectCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/project', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await projectCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.put('/project/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upset: true };
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+          photo: updatedProduct.photo,
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          ratting: updatedProduct.ratting,
+          descriptions: updatedProduct.descriptions,
+        }
+      }
+      const result = await projectCollection.updateOne(filter, product, options);
+      res.send(result);
+    })
+
+    app.delete('/project/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
